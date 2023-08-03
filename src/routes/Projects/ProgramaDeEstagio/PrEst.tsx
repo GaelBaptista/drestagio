7647af24d00fc2933d56dfeaf24ac9126aadda33
Gabriel <rev3npvp@gmail.com>
2023-08-03 09:53:09 -0300

    ProgEsta

--- src/routes/Projects/ProgramaDeEstagio/PrEst.tsx
+++ src/routes/Projects/ProgramaDeEstagio/PrEst.tsx
@@ -1,3 +1,14 @@
 export function PrEst () {
-  return <div>Programa de estagio</div>
+  return (
+    <section className="ContainerProgramadeEstagio">
+  <div className="section-head prEst ">
+          <h4><span>Gestão de </span>DH</h4>
+          <p className='gestaodh'>Promover uma gestão do desenvolvimento humano (DH) significa transformar as vivências e habilidades de
+            cada trabalhador em um recurso muito valioso para o crescimento da empresa e do desenvolvimento dos colaboradores
+
+          </p>
+        </div>
+    </section>
+  
+  )
 }
