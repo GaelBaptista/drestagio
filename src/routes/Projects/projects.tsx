9a1e9df530caf4d520a966a8b4b185a192895a1f
Gabriel <rev3npvp@gmail.com>
2023-08-09 15:50:37 -0300

    remoção

--- src/routes/Projects/projects.tsx
+++ src/routes/Projects/projects.tsx
@@ -1,6 +1,4 @@
-import { Eventos } from "./Esg/eventos"
-import { PrEst } from "./ProgramaDeEstagio/PrEst"
-import { Mais } from "./Localizacao/mais"
+
 import { Outlet } from "react-router-dom";
 export function Projects () {
   return <div><Outlet /></div>
