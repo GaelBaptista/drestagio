bbedfdf5ac63d48484c09b39e233d640f9611f16
Gabriel <rev3npvp@gmail.com>
2023-08-11 16:56:17 -0300

    PrFarmacia

--- src/routes/ProgramaDeEstagio/Farmacia/farmacia.tsx
+++ src/routes/ProgramaDeEstagio/Farmacia/farmacia.tsx
@@ -1,3 +1,62 @@
+import './farmacia.css'
+
 export function Farmacia () {
-  return <h1>fkgdskasdhfj</h1>
+  return (
+    <div id="parallax-world-of-ugg">
+     
+  
+  <section>
+    <div className="title">
+      <h3>Programa de Estágio </h3>
+      <h1>FARMÁCIA</h1>
+    </div>
+  </section>
+  
+  <section>
+      <div className="parallax-one-far">
+        <h2>Trilhando o Caminho Profissional em Farmácia</h2>
+      </div>
+  </section>
+  
+  <section>
+    <div className="block">
+      <p><span className="first-character sc">O</span>estágio é um ato educativo onde são ofertadas vagas para que estudantes dos cursos de farmácia e áreas afins nos níveis superior, técnico ou profissionalizantes possam colocar em prática todo conhecimento teórico adquirido durante seu curso e assim iniciar sua carreira profissional.</p>
+      <p className="line-break margin-top-10"></p>
+      <p className="margin-top-10">Nossos estagiários do Programa de Estágio para Farmácia podem desenvolver atividades em diversos setores como: Atendimento ao cliente no balcão, reposição e organização, operação de caixa, marketing digital, atendimento presencial e online entre outras atividades inerente ao setor aumentando assim a produtividade da sua empresa.</p>
+    </div>
+  </section>
+  
+  <section>
+    <div className="parallax-two-far">
+      <h2>Fortalecendo a Equipe Farmacêutica  </h2>
+    </div>
+  </section>
+  
+  <section>
+    <div className="block">
+      <p><span className="first-character ny">C</span>ontar com estagiários no dia a dia de sua farmácia tem muitos benefícios, isso porque eles têm maior flexibilidade e adaptabilidade para aprender e executar diversas tarefas nos setores </p>
+      <p className="line-break margin-top-10"></p>
+      <p className="margin-top-10">Aqueles que se destacam têm a chance de se tornar funcionários em tempo integral reduzindo os erros de contratação, tempo e custos associados ao recrutamento externo como treinamentos e integração pois eles já estão familiarizados com a cultura da empresa e bem adaptados.</p>
+    </div>
+  </section>
+  
+  
+  <section>
+    <div className="parallax-three-far">
+      <h2> Fomentando Futuros Farmacêuticos</h2>
+    </div>
+  </section>
+  
+  <section>
+    <div className="block">
+      <p><span className="first-character atw">O</span>ferecer oportunidades de estágio mostra o compromisso com o desenvolvimento desses estudantes. Isso pode melhorar a reputação da sua empresa perante a comunidade tornando a sua farmácia um local atraente para que talentos promissores possam desenvolver sua carreira profissional.</p>
+      <p className="line-break margin-top-10"></p>
+      <p className="margin-top-10">Em resumo, a contratação de estagiários pode trazer vantagens substanciais sendo crucial proporcionar um ambiente de trabalho acolhedor e oportunidades significativas de aprendizado para garantir que ambos os lados colham os benefícios máximos dessa colaboração.</p>
+    </div>
+  </section>
+  
+    
+  </div>
+   
+  )
 }
